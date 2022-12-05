86780cd14d672187a4d7af5aee280f2f18a6cdd7
dawid.lipinski <dawid.lipinski@allegro.pl>
2022-12-05 05:28:40 +0100

    Init

--- src/pages/jobs/create.tsx
+++ src/pages/jobs/create.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Spacer, Grid, Text, Image } from '@nextui-org/react'
+import { Spacer, Grid, Text } from '@nextui-org/react'
 import dynamic from 'next/dynamic'
 const  JobForm = dynamic(() => import('../../features/jobs/JobForm'), { ssr: false })
 import image from '../../assets/undraw_uploading_re_okvh(1).svg'
+import Image from 'next/image'
 
 const Create = () =>
 	<>
@@ -12,7 +13,6 @@ const Create = () =>
 		}}>{' for over '}{123} {'developers'}</Text></Text>
 		<Spacer y={1}/>
 		<Grid.Container gap={3}>
-
 			<Grid xs={5}>
 				<Image src={image.src}/>
 			</Grid>
